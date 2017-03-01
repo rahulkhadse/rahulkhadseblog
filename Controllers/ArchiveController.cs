0cf026b38b7d8ec598172846fe4601858741fc75
Rahul Khadse <rahul.a.khadse@gmail.com>
2017-03-01 04:32:49 -0500

    First

---
+++ Controllers/ArchiveController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace rahulkhadseblog.Controllers
+{
+    public class ArchiveController : Controller
+    {
+        public IActionResult Index(string title = "")
+        {
+            if(string.IsNullOrEmpty(title))
+                return RedirectToAction("List"); ;
+            
+            return View("Index", title);
+        }
+
+        public IActionResult List()
+        {
+            return View("List");
+        }
+    }
+}
